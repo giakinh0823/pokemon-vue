17773f7add096de761018bcc719ee2e5089e7aba
giakinh0823 <giakinh2000@gmail.com>
2024-03-31 12:05:35 +0700

    done

---
+++ src/components/ScopyRightScreen.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>
+    This game owned by Hà Gia Kính in Vue 3
+    <a href="#">view here</a>
+  </p>
+</template>
---
+++ src/utils/array.js
@@ -0,0 +1,1 @@
+export const suffled = (list) => list.sort(() => Math.random() - 0.5);
